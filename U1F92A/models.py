464277a7d18072dc9980775db168a2f24f58645d
GefenHajaj <gefenhajaj@gmail.com>
2018-02-10 01:29:25 +0200

    minor conventions change

--- U1F92A/models.py
+++ U1F92A/models.py
@@ -21,7 +21,8 @@ class User(models.Model):
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='maps')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE,
+                                 related_name='maps')
     content_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
     content_text = models.CharField(max_length=1000)
     send_time = models.DateTimeField('date sent')
