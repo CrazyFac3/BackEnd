25a2ca8aa8a7067dde8bad4dc3d166b53c41ce08
GefenHajaj <gefenhajaj@gmail.com>
2018-02-09 23:12:28 +0200

    added __str__ functions to our models

--- U1F92A/models.py
+++ U1F92A/models.py
@@ -6,11 +6,17 @@ class Photo(models.Model):
     url = models.CharField(max_length=1000)
     time_created = models.DateTimeField('Date created')
 
+    def __str__(self):
+        return self.pk
+
 
 class User(models.Model):
     photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
     time_created = models.DateTimeField('date created')
     last_active = models.DateTimeField('last active date')
+    
+    def __str__(self):
+        return self.pk
 
 
 class Message(models.Model):
@@ -19,6 +25,12 @@ class Message(models.Model):
     content_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
     content_text = models.CharField(max_length=1000)
     send_time = models.DateTimeField('date sent')
+
+    def __str__(self):
+        if not self.content_text:
+            return "Photo Message " + str(self.pk)
+        else:
+            return "Text Message " + str(self.pk)
 
 
 
