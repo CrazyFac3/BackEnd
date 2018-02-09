2753ed0916ce2bd9cec6a7de0b0c7842e258c0b0
GefenHajaj <gefenhajaj@gmail.com>
2018-02-09 22:35:29 +0200

    migrated code and added users funcs

--- U1F92A/models.py
+++ U1F92A/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Photo(models.Model):
     url = models.CharField(max_length=1000)
     time_created = models.DateTimeField('Date created')
+
 
 class User(models.Model):
     photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
     time_created = models.DateTimeField('date created')
     last_active = models.DateTimeField('last active date')
 
+
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='maps')
     content_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
     content_text = models.CharField(max_length=1000)
+    send_time = models.DateTimeField('date sent')
 
 
 
--- U1F92A/views.py
+++ U1F92A/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from .models import *
+from django.utils import timezone
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at polls index.")
+
+
+class UserView(View):
+
+    @staticmethod
+    def register(uphoto):
+        """
+        on create - last_activated is the current time.
+        :param uphoto: Photo (U1F92A.models)
+        :return: None
+        """
+        # upload_img(uphoto)
+        new_user = User(photo=uphoto, time_created=timezone.now(), last_active=timezone.now())
+        new_user.save()
+
+    @staticmethod
+    def update_last_active(pk_num):
+        """
+        updating the last_active user attribute
+        :param pk_num: int (id/pk number)
+        :return: None
+        """
+        user = User.objects.get(pk=pk_num)
+        user.last_active = timezone.now()
+
+    @staticmethod
+    def get_all_users():
+        """
+        Returning all users list
+        :return: list
+        """
+        return User.objects.all()
+
+
