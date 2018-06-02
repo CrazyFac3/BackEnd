99368df6d157dd99dfad21a2dc2037cf4822abb4
Gangana3 <organy110@gmail.com>
2018-06-02 22:00:30 +0300

    Adding main page

--- CrazyFace/views.py
+++ CrazyFace/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def main_page(request):
-    return HttpResponse('<h1>Main Page!</h1>')
+    return render(request, 'index.html')
